5f607384aca9729f4eaefab3b5c49a6b75c05bdd
dkareti <danielkareti@gmail.com>
2022-08-09 11:42:30 -0700

    Finalized fibonacci code

--- fibonacci.c
+++ fibonacci.c
@@ -1,8 +1,15 @@
 /*Daniel Kareti*/
 /* Created 08/09/22 */
-/* 1, 3, 5, 8, 13, 21, ... */
+/* 0, 1, 1, 2, 3, 5, 8, 13, 21, ... */
+
 int main(){
     int temporary_var;
-    int i = 1;
-    int j = 3; 
+    int i = 0;
+    int j = 1;
+    while( i < j && j != 21){
+        temporary_v = i;
+        i = j;
+        j = temporary_var + j;
+    } 
+    return 0;
 }
