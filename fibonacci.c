494599007250598b30cd2b0969eb101f1a55ad4d
dkareti <danielkareti@gmail.com>
2022-08-09 11:51:10 -0700

    Added logic of the fibonacci sequence, but there is an issue with the while loop

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,15 @@
 /*Daniel Kareti*/
 /* Created 08/09/22 */
 /* 0, 1, 1, 2, 3, 5, 8, 13, 21, ... */
+#include <stdio.h>
 
 int main(){
     int temporary_var;
     int i = 0;
     int j = 1;
-    while( i < j && j != 21){
-        temporary_v = i;
+    while(j != 21){
+        printf("%d ", i);
+        temporary_var = i;
         i = j;
         j = temporary_var + j;
     } 
