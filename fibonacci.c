8f93903542a4752fce97c822ac36fdea0e780e99
dkareti <danielkareti@gmail.com>
2022-08-10 14:18:02 -0700

    Added comments and formatting to fibonacci code, the one in c and in python

--- fibonacci.c
+++ fibonacci.c
@@ -4,18 +4,22 @@
 #include <stdio.h>
 
 int main(){
+    printf("\n%s\n", "The following numbers are the first thirteen \nvalues in the fibonacci sequence: \n"); 
     int temporary_var;
     int i = 0;
     int j = 1;
-    int count = 0;
+
+    int count = 0; //Variable count used for formatting output
+
     while(j != 377){
-        printf("%d ", i);
+        printf("%s %d: %d\n", "Value number", count+1 , i);
         temporary_var = i;
         
         i = j;
         j += temporary_var; //Can be written as j = temporary_var + j
-        count++;
-        if( count % 5 == 0){
+
+        count += 1;
+        if(count == 13){
             printf("\n");
         }
     }
--- fibonacci.py
+++ fibonacci.py
@@ -3,9 +3,8 @@ j = 1
 
 print("The first 13 values of the fibonacci sequence:")
 while( j != 377):
-     print(i)
-     temporary_var = i
+    print(i)
+    temporary_var = i
 
-     i = j
-     j += temporary_var
-   
+    i = j
+    j += temporary_var
