bbf49903a7621d8c77a978cfb5393f8a0341bc25
dkareti <danielkareti@gmail.com>
2022-08-11 10:14:44 -0700

    Added formatting to the python code of the fibonacci sequence

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,15 @@
 i = 0
 j = 1
+count = 0
+print("\nThe first 13 values of the fibonacci sequence:")
 
-print("The first 13 values of the fibonacci sequence:")
 while( j != 377):
-    print(i)
+    print("Value number " + str(count+1) +":  "+ str(i))
     temporary_var = i
 
     i = j
     j += temporary_var
+    count += 1
+    if count == 13:
+        print()
+    
