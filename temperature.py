e132053feb734a19d5f6004841a0d7ce55521bc9
dkareti <danielkareti@gmail.com>
2022-08-12 12:18:11 -0700

    Finalized the python code for temperature conversion

--- temperature.py
+++ temperature.py
@@ -2,10 +2,14 @@
 
 temp = input("Enter a temperature in degrees Fahrenheit: ")
 
-temp_celsius = ((32 * int(temp)) - 32) * float(5/9)
+temp_celsius = (int(temp) - 32) * float(5/9)
 
 temp_kelvin = temp_celsius + 273.15
 
-print("Your temperature of " + str(temp) + "converted into Celsius and Kelvin is the following:\n")
+#round values to two decimal places
+temp_celsius = round(temp_celsius, 2)
+temp_kelvin = round(temp_kelvin, 2)
+
+print("Your given temperature is " + temp + ". This converted into Celsius and Kelvin is the following:\n")
 print("     Celsius: " + str(temp_celsius))
 print("     Kelvin: " + str(temp_kelvin)) 
