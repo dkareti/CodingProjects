8fb5d4adea8ec731c68557218edf2b90b61e7abf
dkareti <danielkareti@gmail.com>
2022-08-17 12:55:11 -0700

    Added a pattern creator coded in Python

---
+++ pattern.py
@@ -0,0 +1,15 @@
+
+#decreasing pattern
+j = 5;
+while( j > 0):
+    for i in range(j):
+        print("* ", end = ' ')
+    print()
+    j -= 1;
+
+#increasing pattern
+while(j < 6):
+    for k in range(j):
+        print("* ", end = ' ')
+    print()
+    j += 1
