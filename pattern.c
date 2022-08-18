e59e1e58e526d6938ed808a902507b42ac442835
dkareti <danielkareti@gmail.com>
2022-08-18 12:16:37 -0700

    Added code to make a partial square except forgot to account for the extra spaces

--- pattern.c
+++ pattern.c
@@ -2,23 +2,40 @@
 
 int main(){
     int j = 5;
+    int count = 0; //This variable will be used to add to the pattern
 //  Decreasing Pattern (5 Stars --> 1 Star)
     while(j > 0){
         for(int i = 0; i < j; i++){
             printf("%s ", "*");
         }
+        for(int temp = 0; temp < count*2; temp++){
+            printf(" ");
+        } 
+        
+        for(int increment = 0; increment < j; increment++){
+            printf("%s ", "*");
+        }
         printf("\n");
         j -= 1;
+        count += 1;
     }
-//After this block of code j == 1
+//After this block of code j == 1 And count == 4
 
 //  Increasing Pattern (1 Star --> 5 Stars)
     while(j < 6){
         for(int k = 0; k < j; k++){
             printf("%s ", "*");
         }
+        for(int temp2 = 0; temp2 < count*2; temp2++){
+            printf(" ");
+        }
+
+        for(int increment2 = 0; increment2 < j; increment2++){
+             printf("%s ", "*");
+        }
         printf("\n");
         j += 1;
+        count -= 1;
     }
     return 0;
 } 
