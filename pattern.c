f6fbf282f1f974cafd28af37143c602e088063c3
dkareti <danielkareti@gmail.com>
2022-08-18 12:02:13 -0700

    Added code so that a partial rectangle is formed

--- pattern.c
+++ pattern.c
@@ -2,6 +2,7 @@
 
 int main(){
     int j = 5;
+//  Decreasing Pattern (5 Stars --> 1 Star)
     while(j > 0){
         for(int i = 0; i < j; i++){
             printf("%s ", "*");
@@ -9,6 +10,15 @@ int main(){
         printf("\n");
         j -= 1;
     }
-    
-    
+//After this block of code j == 1
+
+//  Increasing Pattern (1 Star --> 5 Stars)
+    while(j < 6){
+        for(int k = 0; k < j; k++){
+            printf("%s ", "*");
+        }
+        printf("\n");
+        j += 1;
+    }
+    return 0;
 } 
