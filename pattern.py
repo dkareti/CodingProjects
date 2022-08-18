1ba24e80fab30c2d5aa5af32cc6afd9c4d6eb1b5
dkareti <danielkareti@gmail.com>
2022-08-18 12:35:10 -0700

    Created a partial square using the triangle patterns of asterix's

--- pattern.py
+++ pattern.py
@@ -1,15 +1,29 @@
 
 #decreasing pattern
 j = 5;
+count = 0;
 while( j > 0):
     for i in range(j):
         print("* ", end = ' ')
+    for index in range(count*3):
+        print(" ", end = ' ')
+    for value in range(j):
+        print("* ", end = ' ')
+
     print()
     j -= 1;
+    count += 1;
+
+#at the end of this block j == 1 And count == 4
 
 #increasing pattern
 while(j < 6):
     for k in range(j):
         print("* ", end = ' ')
+    for index1 in range(count*3):
+        print(" ", end = ' ')
+    for value2 in range(j):
+        print("* ", end = ' ')
     print()
     j += 1
+    count -= 1;
