a0a0fae569650871520e8b71ebd0486ae9354d1a
dkareti <danielkareti@gmail.com>
2022-08-22 12:39:41 -0700

    Created a simple multplication calculator

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!\n";
+     
+    std::cout << "Program Name:\n     Hello Calculator\n";
+    std::cout << "Usage:\n     Muliplies two numbers\n";
+     
+    int x, y;
+    int product;
+    std::cout << "\nEnter two numbers (seperated by one space):";
+    std::cin >> x;
+    std::cin >> y;
+    
+    product = x * y;
+    std::cout << "The product of " << x << " and " << y << " is: " << product;
+    
     return 0;
 }
