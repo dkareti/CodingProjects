bdb5715973b1cb2b3fd45cf9b3db86ffc4cf24d3
dkareti <danielkareti@gmail.com>
2022-08-23 11:36:05 -0700

    Added a do-while loop to the simple calculator to loop the enquiry until the parameters are met

--- hello.cpp
+++ hello.cpp
@@ -3,16 +3,30 @@
 int main(){
      
     std::cout << "Program Name:\n     Hello Calculator\n";
-    std::cout << "Usage:\n     Muliplies two numbers\n";
+    std::cout << "Usage:\n     This program multiplies the two numbers given by the user\n";
      
     int x, y;
     int product;
-    std::cout << "\nEnter two numbers (seperated by one space):";
-    std::cin >> x;
+    bool err = false; //This variable is used to get the correct input
+
+    do{
+        std::cout << "\nEnter a number between 1 and 15: ";
+        std::cin >> x;
+        if(x > 15){
+            fprintf(stderr, "The value given must be between 1 and 15.\n");
+            err = true;
+        }
+        else{
+            err = false;
+        }
+    }
+    while(err);
+    
+    std::cout << "\nEnter another number: ";
     std::cin >> y;
     
     product = x * y;
-    std::cout << "The product of " << x << " and " << y << " is: " << product;
+    std::cout << "\nThe product of " << x << " and " << y << " is: " << product << "\n";
     
     return 0;
 }
