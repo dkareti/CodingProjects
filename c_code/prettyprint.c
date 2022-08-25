7da6049af6b930135dcb598482ab6895b38232f8
dkareti <danielkareti@gmail.com>
2022-08-25 14:10:16 -0700

    Added Error Checks to the prettyprint file

--- c_code/prettyprint.c
+++ c_code/prettyprint.c
@@ -5,12 +5,24 @@
 
 int main(){
     int month, day, year;
-    printf("%s\n", "This program takes the numeric date provided by the user\n and displays the date in a formatted manner");
+    printf("%s\n", "This program takes the numeric date provided by the user\nand displays the date in a formatted manner");
     printf("%s\n", "Enter the current month: ");
     scanf("%d", &month);
+    if(month >12 || month <=0){
+        fprintf(stderr, "Invalid Month Value. Program Terminated\n");
+        return 1;
+    }
 
     printf("%s\n", "Enter the day: ");
     scanf("%d", &day);
+    if( (day > 31 || day <= 0)){
+        fprintf(stderr, "Invalid day Value. Program Terminated\n");
+        return 1;
+    }
+    if(month == 2 || day > 29){ 
+        fprintf(stderr, "Invalid day Value. Program Terminated\n");
+        return 1;
+    }
 
     printf("%s\n", "Enter the current year: ");
     scanf("%d", &year);
