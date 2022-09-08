a4dd5a7352d0687e9c60aec764dd6d8ff0260462
dkareti <danielkareti@gmail.com>
2022-09-08 10:07:48 -0700

    Added a while loop to the star program

--- c++_code/stars.cpp
+++ c++_code/stars.cpp
@@ -4,16 +4,29 @@
 #include <assert.h>
 
 int main(){
-    std::cout << "Enter a number between 5 and 75: ";
     int number;
-    std::cin >> number;
-    assert(number > 5 && number < 75);
 
+    while(true){
+        std::cout << "Enter a number between 5 and 20: ";
+        std::cin >> number;
+        if(number < 5 || number > 20){
+            fprintf(stderr, "Did not input a valid number!\n"); 
+        }
+        else{
+            break;
+        }
+    }
+
+    std::cout << "\nThe provided value is " << number << "\n";
     for(int i = 0; i < number; i++){
-        std::cout << "* ";
+        std::cout << " * ";
     } 
     std::cout << "\n";
-    for(int j = 0; j < number; j++){
+    for(int j = 0; j < number; j+=2){
+        std::cout << " * ";
+    } 
+    std::cout << "\n";
+    for(int k = 0; k < number; k+=3){
         std::cout << " * ";
     } 
     std::cout << "\n";
