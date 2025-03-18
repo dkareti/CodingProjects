9b2933d7bfde5ef181c6dd3eb0bfce80dc89005c
Daniel Kareti <danielkareti@Daniels-MacBook-Air.local>
2025-03-18 13:00:07 -0700

    Added login file

---
+++ python_code/login.py
@@ -0,0 +1,52 @@
+#############
+
+#This file mimics a password interface
+
+############
+
+    
+
+def getPass(array):
+	phrase = str(input("Enter the Login Credential: "))
+	array.append(phrase)
+	return array
+
+def accessLogic(numTries, maxTries):
+	if numTries < maxTries:
+		diff = maxTries - numTries
+		print("You have {} more tries remainining".format(diff))
+		return '1'
+	elif numTries == maxTries:
+		print("Access Denied!")
+		return '0'
+
+	
+
+def main():
+	#create an empty array
+	array1 = []
+
+	attempts = 1
+
+	result = getPass(array1)
+	while result[-1] != "Daniel":
+		print("The access to the underlying system is denied")
+		choice = str(input("\nType 'help' for a Hint!\n Enter Here: \n"))
+		if choice == "help" or choice == "HELP" or choice == "Help":
+			print("Hint : The credential is the Bible character that survives the Lions Den (The first "
+			"character is capitalized, the rest are not.)")
+
+		end_flag = accessLogic(attempts, 10)
+
+		if end_flag == '0' :
+			print("You've successfully authenticated")
+			break
+		else:
+			print("Try again.")
+		result = getPass(array1)
+		attempts += 1
+		
+
+
+if __name__ == '__main__':
+	main()	
